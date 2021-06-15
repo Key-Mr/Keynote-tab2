d94d552671599deffecdfecb42860bbdadc55002
Kirill <tyurikov_kirill@mail.ru>
2021-06-15 12:20:32 +0300

    Mysql credential

--- app.js
+++ app.js
@@ -2,6 +2,14 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const { Sequelize, DataTypes, Model } = require('sequelize');
+
+const port = process.env.PORT || 3000
+
+const sequelize = new Sequelize('t7n1f5tdg8k91gu3', 'x7flgnknnd2tqek2', 'aw8z7nesbr72tnwl', {
+    host: 'yvu4xahse0smimsc.chr7pe7iynqr.eu-west-1.rds.amazonaws.com',
+    dialect: "mysql"
+} )
 
 app.use(express.static(path.join(__dirname, 'public')))
 
